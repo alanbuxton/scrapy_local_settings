27c58d0ac864b7f21197d9b2a7c6d54e078ff0bc
Alan Buxton <alanbuxton@gmail.com>
2018-10-08 13:11:52 +0100

    Simple scrapy project

---
+++ local_settings/spiders/quotes_spider.py
@@ -0,0 +1,18 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        self.logger.info('page %s' % page)
+        self.logger.debug('body %s' % response.text[:500])
+        self.logger.warning('*** WARNING *** WARNING *** WARNING ***')
